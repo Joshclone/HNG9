30fe3414207d066744bafa189f6edc1fe634cc7a
Nwaeze Joshua <nwaezejoshua@gmail.com>
2022-11-02 10:20:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require("express");
-const bodyParser = require('body-parser');
+ const express = require('express');
+const cors = require('cors')
 const app = express();
-ap => console.log(e.message));
+
+const PORT = process.env.PORT || 9000;
+app.use(express.json());
+app.use(cors())
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        
+
+        
+        
+
+            slackUsername: "Nwaeze Joshua",
+            backend: true,
+            age: 
+            bio: "Hi my name is Nwaeze Joshua, I am a backend developer, proficienct in  ExpressNodeJs, l want to learn more and become a skilled software developer providing scalable solutions to applications"
+      
+    })
+})
 
 
 
-
-
-  console.log(`Server is running on port ${port}`);
-
+app.listen(PORT,()=>{
+    console.log(`Server is listening at port ${PORT}`);
+})
